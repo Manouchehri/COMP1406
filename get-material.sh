a6928e2a7b7a72334ad1802e871c7d4def009399
David Manouchehri <manouchehri@riseup.net>
2015-09-11 15:17:14 -0400

    Remove useless variable.

--- get-material.sh
+++ get-material.sh
@@ -4,7 +4,6 @@ PWD="$(pwd)"
 source "${PWD}/auth.txt"
 user_agent="Mozilla/5.0 (Windows NT 6.1; WOW64; rv:39.0) Gecko/20100101 Firefox/39.0"
 url="http://people.scs.carleton.ca/~ldnel/1406fall2015/"
-exclude_url="www.carleton.ca"
 output_folder="website"
 
 wget -e robots=off --convert-links --adjust-extension --page-requisites --continue --timeout=60 --wait=1 --random-wait --user="${username}" --password="${password}" --ignore-length --max-redirect=3 --user-agent="${user_agent}" --no-check-certificate --recursive --level=3 --backup-converted --follow-ftp "${url}" --directory-prefix="${output_folder}/"
