c4065d94e013076d117ffa96eefaee5fbd3aeaa9
David Manouchehri <manouchehri@riseup.net>
2015-10-20 21:53:28 -0400

    Don't restrict the number of levels to fetch.

--- get-material.sh
+++ get-material.sh
@@ -6,4 +6,4 @@ user_agent="Mozilla/5.0 (Windows NT 6.1; WOW64; rv:39.0) Gecko/20100101 Firefox/
 url="http://people.scs.carleton.ca/~ldnel/1406fall2015/"
 output_folder="website"
 
-wget -e robots=off --convert-links --adjust-extension --page-requisites --continue --timeout=60 --wait=1 --random-wait --user="${username}" --password="${password}" --ignore-length --max-redirect=3 --user-agent="${user_agent}" --no-check-certificate --recursive --level=3 --backup-converted --follow-ftp "${url}" --directory-prefix="${output_folder}/"
+wget -e robots=off --convert-links --adjust-extension --page-requisites --continue --timeout=60 --wait=1 --random-wait --user="${username}" --password="${password}" --ignore-length --max-redirect=3 --user-agent="${user_agent}" --no-check-certificate --recursive --backup-converted --follow-ftp "${url}" --directory-prefix="${output_folder}/"
